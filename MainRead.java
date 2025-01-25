c162c639e349c59ff586a80a39d37a1b2b705a87
incomagia <89909141+incomagia@users.noreply.github.com>
2025-01-26 02:15:26 +0300

    Update MainRead.java

--- MainRead.java
+++ MainRead.java
@@ -1,1 +1,27 @@
+import java.util.Scanner;
+
+public class MainRead {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        DataStorage dataStorage = new DataStorage();
+        CommandProcessor commandProcessor = new CommandProcessor(dataStorage);
+
+        System.out.println("Welcome to the Link Shortener Management System!");
+        System.out.println("Type 'help' for a list of commands.");
+
+        while (true) {
+            System.out.print("> ");
+            String input = scanner.nextLine().trim();
+
+            if (input.equalsIgnoreCase("exit")) {
+                System.out.println("Exiting the program. Goodbye!");
+                break;
+            }
+
+            commandProcessor.processCommand(input);
+        }
+
+        scanner.close();
+    }
+}
 
